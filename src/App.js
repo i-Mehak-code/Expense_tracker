da4a7b61a38162d0c4beefe00b160480898d873e
i-Mehak-code <82929979+i-Mehak-code@users.noreply.github.com>
2023-08-17 01:19:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React,{useState} from "react";
+import "./App.css";
+import Expenses from "./Expenses/Expenses";
+import NewExpense from "./NewExpense/NewExpense";
+
+const DUMMY_EXPENSES=[
+  {
+    title: "Car Insurance",
+    amount: "1000000",
+    date: new Date(2022,5,19),
+  },
+  {
+    title: "House Insurance",
+    amount: "1000000",
+    date: new Date(2021,6,19),
+  },
+  {
+    title: "House Insurance",
+    amount: "1000000",
+    date: new Date(2020,6,19),
+  },
+  {
+    title: "Computer Insurance",
+    amount: "1000000",
+    date: new Date(2019,7,19),
+  },
+  {
+    title: "Phone Insurance",
+    amount: "1000000",
+    date: new Date(2021,8,19),
+  },
+];
+
+const App=() => {
+  const [expenses,setExpenses]=useState(DUMMY_EXPENSES);
+  const addExpenseHandler=(expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense,...prevExpenses];
+    });
+  };
+  return (
+    <div className="App">
+
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+};
+
+export default App;
